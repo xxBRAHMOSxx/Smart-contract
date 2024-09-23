022c15d36e44e48406d148e6cc1510563be03ae1
xxBRAHMOSxx <devanshkumar6298@gmail.com>
2024-09-23 12:56:07 +0530

    smart contract

---
+++ SimpleStorage.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19; //version
+
+contract SimpleStorage {
+    uint256 public favoriteNumber;
+
+    struct Person {
+        uint256 nfavouriteNumber;
+        string name;
+    }
+
+    // Person public pat = Person({nfavouriteNumber:7,name:"Pat" });
+    Person[] public listOfPeople;//dynamic array
+
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    mapping(string=>uint256) public nameToFavouriteNumber;
+
+    //view , pure
+    function retrieve() public view returns(uint256){
+        return favoriteNumber;
+    }
+
+    //calldata cant be changed or menupulated, memory can be changed or menupulated
+
+    function addPeople(string memory name,uint256 _favouriteNumber) public {
+
+        listOfPeople.push( Person(_favouriteNumber,name) );
+        nameToFavouriteNumber[name] = _favouriteNumber;
+    }
+
+}
