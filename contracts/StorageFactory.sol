d703bba0fc687634417dccd4c4fcbd244b2d97ef
xxBRAHMOSxx <devanshkumar6298@gmail.com>
2024-09-23 15:41:20 +0530

    contract update

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-import "./SimpleStorage.sol";
+import { SimpleStorage } from "./SimpleStorage.sol";
 
 
 contract StorageFactory{
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContracts;
 
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
         
     }
 }
