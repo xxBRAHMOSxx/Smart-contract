9b650269f638d4ea7c9ab9edb7dc28d6b6d47765
xxBRAHMOSxx <devanshkumar6298@gmail.com>
2024-09-24 15:32:58 +0530

    additional contract utility

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -11,4 +11,13 @@ contract StorageFactory{
         listOfSimpleStorageContracts.push(newSimpleStorageContract);
         
     }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public{
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber);
+    } 
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        return mySimpleStorage.retrieve();
+    }
 }
